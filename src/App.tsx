db7bdbb58cab6914b5d69e4172a6b4e62ab0d539
Aurimas <auruxxas.ar@gmail.com>
2023-06-11 22:05:13 +0300

    remove unused state

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,31 @@ import { useEffect, useState } from "react";
 import CountdownPage from "./containers/CountdownPage";
 import PuzzlePage from "./containers/PuzzlePage";
 
-
 // const RELEASE_DATE = moment("2023-06-13 18:15:00");
 const RELEASE_DATE = moment("2023-06-11 19:29:30");
 
 function App() {
-
   const [isCounting, setIsCounting] = useState(true);
-  const [isSolved, setIsSolved] = useState(false);
 
   useEffect(() => {
     const timeNow = moment();
-    if(timeNow.isAfter(RELEASE_DATE)) {
+    if (timeNow.isAfter(RELEASE_DATE)) {
       setIsCounting(false);
     }
   }, []);
 
-
   const renderPage = () => {
-    if(isCounting) return <CountdownPage date={RELEASE_DATE.toDate()} onComplete={() => setIsCounting(false)} />
-    if(isSolved) return <div>congrats</div>
-    return <PuzzlePage />
-  }
+    if (isCounting)
+      return (
+        <CountdownPage
+          date={RELEASE_DATE.toDate()}
+          onComplete={() => setIsCounting(false)}
+        />
+      );
+    return <PuzzlePage />;
+  };
 
-  return (
-    <div className="App">
-      {renderPage()}
-    </div>
-  );
+  return <div className="App">{renderPage()}</div>;
 }
 
 export default App;
